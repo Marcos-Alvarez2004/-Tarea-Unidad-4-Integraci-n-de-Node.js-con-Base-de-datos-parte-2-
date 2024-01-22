fc7808bd359fb80f271b22101886eb01ea31de68
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-01-22 17:36:26 -0300

    Backend finalizado

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
+var fileUpload = require("express-fileupload");
+var cors = require("cors");
 
 require("dotenv").config();
 
@@ -11,6 +13,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var loginRouter = require("./routes/admin/login");
 var adminRouterNovedades = require("./routes/admin/novedades");
+var apiRouter = require("./routes/api");
 
 var app = express();
 
@@ -23,6 +26,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 app.use(
   session({
@@ -50,6 +59,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/admin/login", loginRouter);
 app.use("/admin/novedades", secured, adminRouterNovedades);
+app.use("/api", cors(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
